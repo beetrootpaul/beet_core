6c2fecdd023cb1b0adf4b4c7b26b256d5eb80702
Beetroot Paul <paul@beetrootpaul.com>
2023-04-19 01:38:20 +0200

    configure for web

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 
 rust_flags_release := RUSTFLAGS="-D warnings"
 
-rust_log_debug := RUST_LOG=debug
+rust_log_debug := RUST_LOG=debug,wgpu_core=info
 
 # # # # # # # # # # #
 # initial commands
@@ -12,6 +12,9 @@ rust_log_debug := RUST_LOG=debug
 
 setup:
 	rustup default stable
+	# wasm-bindgen-cli and miniserve are needed to run WASM examples
+	cargo install --locked wasm-bindgen-cli
+	cargo install --locked miniserve
 
 # # # # # # # # #
 # main commands
@@ -24,9 +27,13 @@ check:
 	cargo clippy --examples
 	cargo clippy --examples --release
 
-run: run_example_tmp
+r: run_example_tmp
 
-run_release: run_example_tmp_release
+rr: run_example_tmp_release
+
+w: run_example_tmp_web
+
+wr: run_example_tmp_web_release
 
 # # # # # # # # # # # # #
 # specialized commands
@@ -36,6 +43,7 @@ update_rust_toolchain:
 	rustup update stable
 
 clean_up:
+	rm -rf ./examples/tmp/wasm-bindgen-output/
 	cargo clean
 
 # # # # # # # # #
@@ -47,3 +55,27 @@ run_example_tmp:
 	$(rust_log_debug) cargo run --example tmp
 run_example_tmp_release:
 	$(rust_flags_release) cargo run --example tmp --release
+run_example_tmp_web:
+	cargo build --example tmp --target wasm32-unknown-unknown
+	mkdir -p ./examples/tmp/wasm-bindgen-output/debug/
+	wasm-bindgen \
+		--target web \
+		--no-demangle \
+		--no-typescript \
+		--out-dir ./examples/tmp/wasm-bindgen-output/debug/ \
+		target/wasm32-unknown-unknown/debug/examples/tmp.wasm
+		cp ./examples/tmp/index.html ./examples/tmp/wasm-bindgen-output/debug/index.html
+	# TODO: [ERROR] Route /favicon.ico could not be found
+	miniserve --port 8080 --index index.html ./examples/tmp/wasm-bindgen-output/debug/
+run_example_tmp_web_release:
+	$(rust_flags_release) cargo build --example tmp --target wasm32-unknown-unknown --release
+	mkdir -p ./examples/tmp/wasm-bindgen-output/release/
+	wasm-bindgen \
+		--target web \
+		--no-demangle \
+		--no-typescript \
+		--out-dir ./examples/tmp/wasm-bindgen-output/release/ \
+		target/wasm32-unknown-unknown/release/examples/tmp.wasm
+		cp ./examples/tmp/index.html ./examples/tmp/wasm-bindgen-output/release/index.html
+	# TODO: [ERROR] Route /favicon.ico could not be found
+	miniserve --port 8080 --index index.html ./examples/tmp/wasm-bindgen-output/release/
--- examples/tmp/main.rs
+++ examples/tmp/main.rs
@@ -5,9 +5,11 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
+use std::rc::Rc;
+
 use error_iter::ErrorIter as _;
 use log::{debug, error, trace};
-use pixels::{Error, Pixels, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
 use winit::{
     dpi::LogicalSize,
     event::{Event, VirtualKeyCode},
@@ -24,20 +26,36 @@ const HEIGHT: u32 = 300;
 // TODO: rename this example package
 
 // TODO: reuse this
-// const WINDOW_TITLE: &str = "Beet Core: \"Minimal\" examples";
+// const WINDOW_TITLE: &str = "Beet Core: \"Minimal\" example";
 // const CANVAS_WIDTH: u32 = 64;
 // const CANVAS_HEIGHT: u32 = 32;
 // const CANVAS_SCALE: u32 = 8;
 // const WINDOW_SIZE: LogicalSize<u32> =
 //     LogicalSize::new(CANVAS_WIDTH * CANVAS_SCALE, CANVAS_HEIGHT * CANVAS_SCALE);
 
-fn main() -> Result<(), Error> {
+fn main() {
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        // TODO: configure log level from outside the code and per debug vs release
+        console_log::init_with_level(log::Level::Warn).expect("error initializing logger");
+
+        wasm_bindgen_futures::spawn_local(run());
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+
+        pollster::block_on(run());
+    }
+
     // TODO: change this
     BeetCore::hello();
+}
 
-    env_logger::init();
+async fn run() {
     let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
 
     let window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
@@ -49,11 +67,60 @@ fn main() -> Result<(), Error> {
             .build(&event_loop)
             .unwrap()
     };
+    let window = Rc::new(window);
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowExtWebSys;
+
+        // Retrieve current width and height dimensions of browser client window
+        let get_window_size = || {
+            let client_window = web_sys::window().unwrap();
+            LogicalSize::new(
+                client_window.inner_width().unwrap().as_f64().unwrap(),
+                client_window.inner_height().unwrap().as_f64().unwrap(),
+            )
+        };
+
+        let window = Rc::clone(&window);
+
+        // Initialize winit window with current dimensions of browser client
+        window.set_inner_size(get_window_size());
+
+        let client_window = web_sys::window().unwrap();
+
+        // Attach winit canvas to body element
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+
+        // Listen for resize event on browser client. Adjust winit window dimensions
+        // on event trigger
+        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+            let size = get_window_size();
+            window.set_inner_size(size)
+        }) as Box<dyn FnMut(_)>);
+        client_window
+            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+            .unwrap();
+        closure.forget();
+    }
+
+    let mut input = WinitInputHelper::new();
 
     let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
+        Pixels::new_async(WIDTH, HEIGHT, surface_texture)
+            .await
+            .expect("Pixels error")
     };
 
     let mut life = ConwayGrid::new_random(WIDTH as usize, HEIGHT as usize);
