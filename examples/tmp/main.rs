cd69561b541e9151cf1fa1ba106f5e496a60e76d
Beetroot Paul <paul@beetrootpaul.com>
2023-04-22 23:47:15 +0200

    wip simplification 7

--- examples/tmp/main.rs
+++ examples/tmp/main.rs
@@ -8,7 +8,6 @@ use std::rc::Rc;
 use log::{debug, error, warn};
 use pixels::{Pixels, SurfaceTexture};
 use wasm_bindgen::JsCast;
-use winit::dpi::LogicalPosition;
 use winit::event::VirtualKeyCode;
 use winit::platform::web::WindowBuilderExtWebSys;
 use winit::{
@@ -69,7 +68,8 @@ impl GameApp for TmpGame {
         self.blue = (self.blue as i32 + if self.blue_up { 1 } else { -1 }) as u8;
 
         self.iters += 10;
-        if self.iters > 1_000 {
+        // TODO: make it configurable with keyboard buttons
+        if self.iters > 100 {
             self.iters = 10;
         }
         self.red_px += 1;
@@ -79,10 +79,10 @@ impl GameApp for TmpGame {
     }
 
     fn draw(&mut self, draw_api: &mut DrawApi) {
-        web_sys::console::log_1(&format!("it {}", self.iters).into());
-        for _ in 0..self.iters {
-            draw_api.fill([0x00, 0xff - self.blue, self.blue, 0xff]);
-        }
+        // web_sys::console::log_1(&format!("it {}", self.iters).into());
+        // for _ in 0..self.iters {
+        draw_api.fill([0x00, 0xff - self.blue, self.blue, 0xff]);
+        // }
         draw_api.set_px(self.red_px, [0xff, 0x00, 0x00, 0xff]);
     }
 }
@@ -246,7 +246,7 @@ async fn run<A: GameApp + 'static>() {
     };
     let mut draw_api = DrawApi::new(pixels);
 
-    let mut paused = false;
+    let mut debug_pause = false;
 
     // TODO: use `if window_id == window.id()` match branch condition
 
@@ -262,6 +262,8 @@ async fn run<A: GameApp + 'static>() {
 
     let mut prev_now = performance.now();
 
+    let mut resume_for_1_frame = false;
+
     event_loop.run(move |event, _, control_flow| {
         // Poll is recommended for games. See: https://docs.rs/winit/0.28.3/winit/#event-handling
         control_flow.set_poll();
@@ -276,14 +278,19 @@ async fn run<A: GameApp + 'static>() {
             prev_now = now;
 
             while accumulated_delta > EXPECTED_DELTA {
-                web_sys::console::log_1(&format!("> aUd {}", accumulated_delta).into());
+                // web_sys::console::log_1(&format!("> ad {}", accumulated_delta).into());
                 if accumulated_delta > 2.0 * EXPECTED_DELTA {
-                    error!("LONG UPDATE: {} >> {}", accumulated_delta, EXPECTED_DELTA);
+                    error!(
+                        "LONG UPDATE: {} (expected: {})",
+                        accumulated_delta, EXPECTED_DELTA
+                    );
                 }
 
-                if !paused {
+                // TODO: configure key from the outside
+                if !debug_pause || resume_for_1_frame {
                     game_app.update();
                 }
+                resume_for_1_frame = false;
 
                 if accumulated_delta < 2.0 * EXPECTED_DELTA {
                     game_app.draw(&mut draw_api);
@@ -311,49 +318,26 @@ async fn run<A: GameApp + 'static>() {
 
         // For everything else, for let winit_input_helper collect events to build its state.
         // It returns `true` when it is time to update our game state and request a redraw.
+        warn!("???");
         if input.update(&event) {
-            // TODO: ??? does it make sense at all for WASM ???
-            // if input.key_pressed(VirtualKeyCode::Escape)
-            //     || input.close_requested()
-            //     || input.destroyed()
-            // {
-            //     *control_flow = ControlFlow::Exit;
-            //     return;
-            // }
-
-            if input.key_pressed(VirtualKeyCode::P) {
-                paused = !paused;
+            // TODO: make it configurable from the outside
+            // This allows to stop the game in a browser just by pressing Esc
+            #[cfg(debug_assertions)]
+            if input.key_pressed(VirtualKeyCode::Escape) {
+                *control_flow = ControlFlow::Exit;
+                return;
             }
-            // if input.key_pressed_os(VirtualKeyCode::Space) {
-            // Space is frame-step, so ensure we're paused
-            // paused = true;
-            // }
-            // if input.key_pressed(VirtualKeyCode::R) {
-            //     life.randomize();
-            // }
-            // Handle mouse. This is a bit involved since support some simple
-            // line drawing (mostly because it makes nice looking patterns).
-            // let (mouse_cell, mouse_prev_cell) = input
-            //     .mouse()
-            //     .map(|(mx, my)| {
-            //         let (dx, dy) = input.mouse_diff();
-            //         let prev_x = mx - dx;
-            //         let prev_y = my - dy;
-            //
-            //         let (mx_i, my_i) = pixels
-            //             .window_pos_to_pixel((mx, my))
-            //             .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-            //
-            //         let (px_i, py_i) = pixels
-            //             .window_pos_to_pixel((prev_x, prev_y))
-            //             .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-            //
-            //         (
-            //             (mx_i as isize, my_i as isize),
-            //             (px_i as isize, py_i as isize),
-            //         )
-            //     })
-            //     .unwrap_or_default();
+
+            // TODO: make it configurable from the outside
+            #[cfg(debug_assertions)]
+            if input.key_pressed(VirtualKeyCode::Period) {
+                debug_pause = !debug_pause;
+            }
+            #[cfg(debug_assertions)]
+            if input.key_pressed_os(VirtualKeyCode::Comma) {
+                resume_for_1_frame = true;
+                debug_pause = true;
+            }
 
             // Resize the window
             // if let Some(size) = input.window_resized() {
@@ -362,9 +346,6 @@ async fn run<A: GameApp + 'static>() {
             //         *control_flow = ControlFlow::Exit;
             //         return;
             //     }
-            // }
-            // if !paused || input.key_pressed_os(VirtualKeyCode::Space) {
-            //     life.update();
             // }
 
             winit_window.request_redraw();
@@ -379,199 +360,3 @@ fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
     //         error!("  Caused by: {source}");
     //     }
 }
-
-// fn generate_seed() -> (u64, u64) {
-//     use byteorder::{ByteOrder, NativeEndian};
-//     use getrandom::getrandom;
-
-// let mut seed = [0_u8; 16];
-
-// getrandom(&mut seed).expect("failed to getrandom");
-//
-// (
-//     NativeEndian::read_u64(&seed[0..8]),
-//     NativeEndian::read_u64(&seed[8..16]),
-// )
-// }
-
-// const BIRTH_RULE: [bool; 9] = [false, false, false, true, false, false, false, false, false];
-// const SURVIVE_RULE: [bool; 9] = [false, false, true, true, false, false, false, false, false];
-// const INITIAL_FILL: f32 = 0.3;
-
-// #[derive(Clone, Copy, Debug, Default)]
-// struct Cell {
-//     alive: bool,
-// Used for the trail effect. Always 255 if `self.alive` is true (We could
-// use an enum for Cell, but it makes several functions slightly more
-// complex, and doesn't actually make anything any simpler here, or save any
-// memory, so we don't)
-// heat: u8,
-// }
-
-// impl Cell {
-//     fn new(alive: bool) -> Self {
-//         Self { alive, heat: 0 }
-//     }
-//
-//     #[must_use]
-//     fn update_neibs(self, n: usize) -> Self {
-//         let next_alive = if self.alive {
-//             SURVIVE_RULE[n]
-//         } else {
-//             BIRTH_RULE[n]
-//         };
-//         self.next_state(next_alive)
-//     }
-//
-//     #[must_use]
-//     fn next_state(mut self, alive: bool) -> Self {
-//         self.alive = alive;
-//         if self.alive {
-//             self.heat = 255;
-//         } else {
-//             self.heat = self.heat.saturating_sub(1);
-//         }
-//         self
-//     }
-//
-//     fn set_alive(&mut self, alive: bool) {
-//         *self = self.next_state(alive);
-//     }
-//
-//     fn cool_off(&mut self, decay: f32) {
-//         if !self.alive {
-//             let heat = (self.heat as f32 * decay).clamp(0.0, 255.0);
-//             assert!(heat.is_finite());
-//             self.heat = heat as u8;
-//         }
-//     }
-// }
-
-// #[derive(Clone, Debug)]
-// struct ConwayGrid {
-//     cells: Vec<Cell>,
-//     width: usize,
-//     height: usize,
-// Should always be the same size as `cells`. When updating, we read from
-// `cells` and write to `scratch_cells`, then swap. Otherwise it's not in
-// use, and `cells` should be updated directly.
-// scratch_cells: Vec<Cell>,
-// }
-
-// impl ConwayGrid {
-//     fn new_empty(width: usize, height: usize) -> Self {
-//         assert!(width != 0 && height != 0);
-//         let size = width.checked_mul(height).expect("too big");
-//         Self {
-//             cells: vec![Cell::default(); size],
-//             scratch_cells: vec![Cell::default(); size],
-//             width,
-//             height,
-//         }
-//     }
-//
-//     fn new_random(width: usize, height: usize) -> Self {
-//         let mut result = Self::new_empty(width, height);
-//         result.randomize();
-//         result
-//     }
-//
-//     fn randomize(&mut self) {
-//         let mut rng: randomize::PCG32 = generate_seed().into();
-//         for c in self.cells.iter_mut() {
-//             let alive = randomize::f32_half_open_right(rng.next_u32()) > INITIAL_FILL;
-//             *c = Cell::new(alive);
-//         }
-// run a few simulation iterations for aesthetics (If we don't, the
-// noise is ugly)
-// for _ in 0..3 {
-//     self.update();
-// }
-// Smooth out noise in the heatmap that would remain for a while
-// for c in self.cells.iter_mut() {
-//     c.cool_off(0.4);
-// }
-// }
-
-// fn count_neibs(&self, x: usize, y: usize) -> usize {
-//     let (xm1, xp1) = if x == 0 {
-//         (self.width - 1, x + 1)
-//     } else if x == self.width - 1 {
-//         (x - 1, 0)
-//     } else {
-//         (x - 1, x + 1)
-//     };
-//     let (ym1, yp1) = if y == 0 {
-//         (self.height - 1, y + 1)
-//     } else if y == self.height - 1 {
-//         (y - 1, 0)
-//     } else {
-//         (y - 1, y + 1)
-//     };
-//     self.cells[xm1 + ym1 * self.width].alive as usize
-//         + self.cells[x + ym1 * self.width].alive as usize
-//         + self.cells[xp1 + ym1 * self.width].alive as usize
-//         + self.cells[xm1 + y * self.width].alive as usize
-// /            + self.cells[xp1 + y * self.width].alive as usize
-//             + self.cells[xm1 + yp1 * self.width].alive as usize
-//             + self.cells[x + yp1 * self.width].alive as usize
-//             + self.cells[xp1 + yp1 * self.width].alive as usize
-//     }
-
-// fn update(&mut self) {
-//     for y in 0..self.height {
-//         for x in 0..self.width {
-//             let neibs = self.count_neibs(x, y);
-//             let idx = x + y * self.width;
-//             let next = self.cells[idx].update_neibs(neibs);
-// Write into scratch_cells, since we're still reading from `self.cells`
-// self.scratch_cells[idx] = next;
-// }
-// }
-// std::mem::swap(&mut self.scratch_cells, &mut self.cells);
-// }
-
-// fn toggle(&mut self, x: isize, y: isize) -> bool {
-//     if let Some(i) = self.grid_idx(x, y) {
-//         let was_alive = self.cells[i].alive;
-//         self.cells[i].set_alive(!was_alive);
-//         !was_alive
-//     } else {
-//         false
-//     }
-// }
-
-// fn draw(&self, screen: &mut [u8]) {
-//     debug_assert_eq!(screen.len(), 4 * self.cells.len());
-//     for (c, pix) in self.cells.iter().zip(screen.chunks_exact_mut(4)) {
-//         let color = if c.alive {
-//             [0, 0xff, 0xff, 0xff]
-//         } else {
-//             [0, 0, c.heat, 0xff]
-//         };
-//         pix.copy_from_slice(&color);
-//     }
-// }
-
-// fn set_line(&mut self, x0: isize, y0: isize, x1: isize, y1: isize, alive: bool) {
-// probably should do sutherland-hodgeman if this were more serious.
-// instead just clamp the start pos, and draw until moving towards the
-// end pos takes us out of bounds.
-// let x0 = x0.clamp(0, self.width as isize);
-// let y0 = y0.clamp(0, self.height as isize);
-// for (x, y) in line_drawing::Bresenham::new((x0, y0), (x1, y1)) {
-//     if let Some(i) = self.grid_idx(x, y) {
-//         self.cells[i].set_alive(alive);
-//     } else {
-//         break;
-//     }
-// }
-// }
-
-// fn grid_idx<I: std::convert::TryInto<usize>>(&self, x: I, y: I) -> Option<usize> {
-//     match (x.try_into(), y.try_into()) {
-//         (Ok(x), Ok(y)) if x < self.width && y < self.height => Some(x + y * self.width),
-//         _ => None,
-//     }
-// }
-// }
